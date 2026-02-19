02420dd28ff24dc380d5b13773ec4a6c8a568133
Eva Duston-Fursman <eva.duston.fursman@gmail.com>
2026-02-19 18:22:44 +1300

    Rename

--- models/response.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Response struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
-}
