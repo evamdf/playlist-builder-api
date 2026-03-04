0211b8ee5adb031b2d08e2b310069a570e7d951c
evamdf <eva.duston.fursman@gmail.com>
2026-03-04 18:04:31 +1300

    Add database to health check

--- handlers/health.go
+++ handlers/health.go
@@ -2,14 +2,25 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/evamdf/api-project/database"
 	"github.com/evamdf/api-project/models"
 	"github.com/gin-gonic/gin"
 )
 
 func HealthCheck(c *gin.Context) {
+	dbStatus := "connected"
+	if database.DB == nil {
+		dbStatus = "not connected"
+	}
+
 	c.JSON(http.StatusOK, models.Response{
 		Success: true,
 		Message: "API is running",
+		Data: map[string]interface{}{
+			"database": dbStatus,
+			"time":     time.Now().Format(time.RFC3339),
+		},
 	})
 }
