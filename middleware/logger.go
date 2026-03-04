df414272fddb091d30a032eaf7c04f390347847e
evamdf <eva.duston.fursman@gmail.com>
2026-03-04 18:09:21 +1300

    Add client IP to logger

--- middleware/logger.go
+++ middleware/logger.go
@@ -11,11 +11,12 @@ func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		c.Next()
-		log.Printf("%s %s %d %s",
+		log.Printf("%s %s %d %s %s",
 			c.Request.Method,
 			c.Request.URL.Path,
 			c.Writer.Status(),
 			time.Since(start),
+			c.ClientIP(),
 		)
 	}
 }
